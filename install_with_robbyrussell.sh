ded2b9f12ee68e2bd006c08b556e69f1f2eba695
biga <bigazzzz@gmail.com>
2020-04-24 17:32:03 +0300

    install environment script. second iteration

--- install_with_robbyrussell.sh
+++ install_with_robbyrussell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt update
+sudo apt update && apt full-upgrade -y && apt autoremove -y
 sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-myzsh/custom}/plugins/zsh-autosuggestions
@@ -8,9 +8,9 @@ cp "$ZSH/themes/robbyrussell.zsh-theme" "$ZSH_CUSTOM/themes/robbyrussell.zsh-the
 echo '[*] replace "%c" with "%(4~|%-1~/…/%2~|%3~)" in "$ZSH_CUSTOM/themes/robbyrussell.zsh-theme"'
 sudo apt install -y tmux vim mc 
 sudo update-alternatives --set editor `which vim.tiny`
-git clone https://github.com/bigazzzz/my_term_tools.git /tmp/
-mv /tmp/my_term_tools/configs ~/
-rm -r /tmp/my_term_tools
+mv my_term_tools/configs ~/
+cd ..
+rm -r my_term_tools
 
 
 
