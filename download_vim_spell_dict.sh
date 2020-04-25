ffbed252a6349451300425bba6fe0f89438d1d27
biga <bigazzzz@gmail.com>
2020-04-25 23:05:44 +0300

    refactoring

---
+++ download_vim_spell_dict.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir ~/.vim/spell
+cd ~/.vim/spell
+wget http://ftp.vim.org/vim/runtime/spell/en.utf-8.spl
+wget http://ftp.vim.org/vim/runtime/spell/en.utf-8.sug
+wget http://ftp.vim.org/vim/runtime/spell/ru.utf-8.spl
+wget http://ftp.vim.org/vim/runtime/spell/ru.utf-8.sug 
