e7470eb0311b5be2d13870670968c5a03e1c7b9f
biga <bigazzzz@gmail.com>
2020-04-29 14:00:39 +0300

    install.sh finalizied

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ sudo apt install fonts-powerline -y
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 cp $MY_CONFIG_DIR/.zshrc_my ~/
-cp $MY_CONFIG_DIR/.p10k.zsh ~/
 echo "source ~/.zshrc_my" >> ~/.zshrc
+p10k configure
 source ~/.zshrc
 
 # TMUX 
