3720eb27e81c2f5e2a7ee2b122459b22cf3eb261
bigazzzz <bigazzzz@gmail.com>
2020-05-19 19:45:51 +0300

    install remastered

--- install.sh
+++ install.sh
@@ -3,30 +3,24 @@
 MY_CONFIG_DIR=`dirname $0`/configs
 
 # MC
-sudo apt update && sudo apt install mc -y
-
+sudo apt update && sudo apt install mc tmux fonts-powerline git zsh vim -y
 # ZSH && oh-my-zsh
-sudo apt install zsh -y && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # ZSH plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions sudo web-search)/g' ~/.zshrc
 # ZSH themes
-sudo apt install fonts-powerline -y
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+#sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $MY_CONFIG_DIR/.zshrc_my ~/
 echo "source ~/.zshrc_my" >> ~/.zshrc
-p10k configure
 source ~/.zshrc
 
 # TMUX 
-sudo apt install -y tmux
 cp $MY_CONFIG_DIR/.tmux.conf ~/
 
 # VIM
-sudo apt install -y vim 
 sudo update-alternatives --set editor `which vim.tiny`
 VIM_SPELL="~/.vim/spell"
 mkdir -p $VIM_SPELL
