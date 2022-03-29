ca7fd662e5c5a4d3d8c69438e575272921b05967
bigazzzz <bigazzzz@gmail.com>
2022-03-29 23:28:07 +0300

    remove ubuntu dependencies

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 MY_CONFIG_DIR=`dirname $0`/configs
 
-# MC
-sudo apt update && sudo apt install mc tmux fonts-powerline zsh vim -y
-# ZSH && oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --skip-chsh"
 # ZSH plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
